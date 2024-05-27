3dd597d808b77b07fb4f70edbb360708852fc43f
raquelbytes <raquellopezgonzalez21@gmail.com>
2024-05-27 22:11:03 +0200

    Creo la aplicación de escritorio

---
+++ Escritorio/GrapeGuardAdminPanel/src/main/java/UD3_ProyectoBase.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+
+/**
+ *
+ * @author Acceso a datos
+ */
+public class UD3_ProyectoBase {
+
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ Escritorio/GrapeGuardAdminPanel/src/main/java/modelo/dao/Dao.java
@@ -0,0 +1,17 @@
+package modelo.dao;
+
+//import java.sql.Connection;
+//import java.util.List;
+
+
+public interface Dao<T> {
+    
+    //public List<T> getAll(Connection conn);
+    
+   // public void add(T t, Connection conn);
+    
+   // public boolean update(T t, String[] params,Connection conn);
+    
+   // public boolean delete(T t, Connection conn);
+
+}
